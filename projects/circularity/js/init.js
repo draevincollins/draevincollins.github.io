07f3ecf224a837f88535ea598a6feba58096b5dd
draevincollins <157414810+draevincollins@users.noreply.github.com>
2024-05-09 19:21:49 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -2,12 +2,15 @@ var init = function (window) {
   "use strict";
   var draw = window.opspark.draw,
     physikz = window.opspark.racket.physikz,
+   
     app = window.opspark.makeApp(),
     canvas = app.canvas,
     view = app.view,
     fps = draw.fps("#000");
 
+
   window.opspark.makeGame = function () {
+   
     window.opspark.game = {};
     var game = window.opspark.game;
 
@@ -24,16 +27,14 @@ var init = function (window) {
       physikz.addRandomVelocity(circle, canvas, 2.5, 2.5);
       view.addChild(circle);
       circles.push(circle);
-      physikz.updatePosition(circle);
     }
 
     // TODO 3 / 7 : Call the drawCircle() function
-    drawCircle(1);
-    drawCircle(2);
-    drawCircle(3);
-    drawCircle(4);
-    drawCircle(5);
-
+    var loopsCompleted = 0;
+    while (loopsCompleted < 100) {
+      drawCircle()
+      loopscomleted++;
+    }
     ////////////////////////////////////////////////////////////
     ///////////////// PROGRAM LOGIC ////////////////////////////
     ////////////////////////////////////////////////////////////
@@ -45,12 +46,16 @@ var init = function (window) {
         */
     function update() {
       // TODO 4 : Update the circle's position //
-      physikz.updatePosition(drawCircle[0]);
-      physikz.updatePosition(drawCircle[1]);
-      physikz.updatePosition(drawCircle[2]);
-      physikz.updatePosition(drawCircle[3]);
-      physikz.updatePosition(drawCircle[4]);
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i])
+        game.checkCirclePosition(circles[i])
+      }
       // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
+      game.checkCirclePosition(circles[0]);
+      game.checkCirclePosition(circles[1]);
+      game.checkCirclePosition(circles[2]);
+      game.checkCirclePosition(circles[3]);
+      game.checkCirclePosition(circles[4]);
       // TODO 9 : Iterate over the array
     }
 
@@ -64,11 +69,19 @@ var init = function (window) {
       if (circle.x > canvas.width) {
         circle.x = 0;
       }
+      if (circle.x < 0) {
+        circle.x = canvas.width
+      }
 
       // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-
+      if (circle.y > canvas.height) {
+        circle.y = 1;
+      }
+      if (circle.y < 0) {
+        circle.y = canvas.height
+      }
       // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-    };
+    }
 
     /////////////////////////////////////////////////////////////
     // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
@@ -87,9 +100,8 @@ var init = function (window) {
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if (
-  typeof process !== "undefined" &&
-  typeof process.versions.node !== "undefined"
+if ((typeof process !== "undefined") &&
+(typeof process.versions.node !== "undefined")
 ) {
   // here, export any references you need for tests //
   module.exports = init;
